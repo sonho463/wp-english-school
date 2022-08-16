d43a099cc02763db2fab52d9d94f6764211507c0
Son Hong Su <son.ho463@gmail.com>
2022-08-16 15:08:58 +0900

    priceページの矢印

--- page-price.php
+++ page-price.php
@@ -166,45 +166,16 @@ get_template_part('includes/header');
 						</tbody>
 					</table>
 					<div class="price-button-next js-price-button-next">
-						<img src="./assets/images/arrow-right.png" alt="">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/arrow-right.png" alt="">
 					</div>
 				</section>
 			</div>
 		</section>
 	</section>
 
-	<section class="l-cta">
-		<div class="p-cta">
-			<div class="c-inner">
-				<h2 class="p-cta__heading c-section__title">
-					まずは無料で資料請求から
-				</h2>
-				<div class="p-cta__button">
-					<a href="#" class="c-button">
-						<span class="c-button__text">資料請求</span>
-					</a>
-				</div>
-				<a class="c-link-contact">
-					<span class="c-link-contact__text">
-						お問い合わせ
-					</span>
-				</a>
-			</div>
-		</div>
-	</section>
-	<section class="l-tel">
-		<div class="p-tel">
-			<div class="c-inner">
-				<span class="p-tel__heading">お電話でのお問い合わせはこちら</span>
-				<a href="tel:0123-456-7890">
-					<span class="p-tel__number">
-						0123-456-7890
-					</span>
-				</a>
-				<span class="p-tel__time">平日 08:00~20:00</span>
-			</div>
-		</div>
-	</section>
+	<?php
+	get_template_part('includes/cta');
+	?>
 
 </main>
 
