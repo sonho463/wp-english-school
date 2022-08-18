8b77e8a4054451a52c33c1b5f5cd996337897718
Son Hong Su <son.ho463@gmail.com>
2022-08-18 16:16:26 +0900

    ctaコンポーネント

--- single-news.php
+++ single-news.php
@@ -4,7 +4,7 @@ get_template_part('includes/header');
 ?>
 
 <main class="l-main">
-<section class="c-breadcrumbs">
+	<section class="c-breadcrumbs">
 		<?php
 		get_template_part('includes/breadcrumb');
 		?>
@@ -33,38 +33,9 @@ get_template_part('includes/header');
 
 
 
-	<section class="l-cta">
-		<div class="p-cta">
-			<div class="c-inner">
-				<h2 class="p-cta__heading c-section__title">
-					まずは無料で資料請求から
-				</h2>
-				<div class="p-cta__button">
-					<a href="#" class="c-button">
-						<span class="c-button__text">資料請求</span>
-					</a>
-				</div>
-				<a class="c-link-contact">
-					<span class="c-link-contact__text">
-						お問い合わせ
-					</span>
-				</a>
-			</div>
-		</div>
-	</section>
-	<section class="l-tel">
-		<div class="p-tel">
-			<div class="c-inner">
-				<span class="p-tel__heading">お電話でのお問い合わせはこちら</span>
-				<a href="tel:0123-456-7890">
-					<span class="p-tel__number">
-						0123-456-7890
-					</span>
-				</a>
-				<span class="p-tel__time">平日 08:00~20:00</span>
-			</div>
-		</div>
-	</section>
+	<?php
+	get_template_part('includes/cta');
+	?>
 </main>
 <?php
 get_template_part('includes/footer');
